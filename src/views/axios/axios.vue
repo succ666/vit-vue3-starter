5b06da6ba024a2cc6a035ee5300f1c558f4e33f5
succ666 <2014649323@qq.com>
2021-12-28 21:59:57 +0800

    vit-vue3-ts基础框架

---
+++ src/views/axios/axios.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>axios</div>
+</template>
+
+<script>
+export default {
+  name: 'Axios'
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/vuex/vuex.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>vuex</div>
+</template>
+
+<script>
+export default {
+  name: 'Vuex'
+}
+</script>
+
+<style scoped></style>
